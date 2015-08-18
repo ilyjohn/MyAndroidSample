7875bbdc714e7588148898a93f7ed2c434b0b61e
panlin <johnjohn7666@hotmail.com>
2015-08-18 17:55:00 +0800

    na

--- app/src/main/java/com/example/panlin_pan/myapplication/starsmenu/StarsHolderActivity.java
+++ app/src/main/java/com/example/panlin_pan/myapplication/starsmenu/StarsHolderActivity.java
@@ -1,33 +1,102 @@
 package com.example.panlin_pan.myapplication.starsmenu;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.panlin_pan.myapplication.R;
 
 import java.util.ArrayList;
 
 public class StarsHolderActivity extends ActionBarActivity {
+    StarsView starsView1, starsView2, starsView3, starsView4;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stars_holder);
 
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i <20 ; i++) {
-            arrayList.add(i);
-        }
+        lv = (ListView) findViewById(R.id.listView);
+        starsView1 = (StarsView) findViewById(R.id.starsMenu1);
+        starsView2 = (StarsView) findViewById(R.id.starsMenu2);
+        starsView3 = (StarsView) findViewById(R.id.starsMenu3);
+        starsView4 = (StarsView) findViewById(R.id.starsMenu4);
 
-        ListView lv =(ListView) findViewById(R.id.listView);
-        ArrayAdapter<Integer> array2ListView =new ArrayAdapter<Integer>(this,android.R.layout.simple_list_item_activated_1,arrayList);
+        initEvents();
+
+        initListView();
+
+        animateListView();
+    }
+
+    private void initListView() {
+        ArrayList<Character> arrayList = new ArrayList<>();
+        for (int i = 'A'; i < 'Z'; i++) {
+            arrayList.add((char) i);
+        }
+        ArrayAdapter<Character> array2ListView = new ArrayAdapter<Character>(this
+                , android.R.layout.simple_list_item_activated_1, arrayList);
         lv.setAdapter(array2ListView);
 
+        Button btn = new Button(this);
+        btn.setText("header");
+        lv.addHeaderView(btn);
+    }
+
+    private void animateListView() {
+        LayoutAnimationController controller = new LayoutAnimationController(
+                AnimationUtils.loadAnimation(this, R.anim.abc_slide_in_top));
+        controller.setOrder(LayoutAnimationController.ORDER_REVERSE);
+        controller.setDelay(1.5f);
+        lv.setLayoutAnimation(controller);
+        lv.startLayoutAnimation();
+    }
+    public StarsView.OnMenuItemClickListener onMenuItemClick =new StarsView.OnMenuItemClickListener() {
+        @Override
+        public void onMenuItemClick(View v, int pos) {
+            Toast.makeText(StarsHolderActivity.this, pos + ":" + v.getTag(), Toast.LENGTH_SHORT).show();
+
+        }
+    };
+    private void initEvents() {
+        starsView1.setOnMenuItemClickListener(onMenuItemClick);
+        starsView2.setOnMenuItemClickListener(onMenuItemClick);
+        starsView3.setOnMenuItemClickListener(onMenuItemClick);
+        starsView4.setOnMenuItemClickListener(onMenuItemClick);
+
+        lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem,
+                                 int visibleItemCount, int totalItemCount) {
+                if (starsView1.isOpen)
+                    starsView1.toggleStarsMenu(1600);
+
+                if (starsView2.isOpen)
+                    starsView2.toggleStarsMenu(1600);
+
+                if (starsView3.isOpen)
+                    starsView3.toggleStarsMenu(1600);
+
+                if (starsView4.isOpen)
+                    starsView4.toggleStarsMenu(1600);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/panlin_pan/myapplication/starsmenu/StarsView.java
+++ app/src/main/java/com/example/panlin_pan/myapplication/starsmenu/StarsView.java
@@ -1,15 +1,31 @@
 package com.example.panlin_pan.myapplication.starsmenu;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+
+import com.example.panlin_pan.myapplication.R;
 
 /**
  * Created by panlin_pan on 8/14/2015.
  */
-public class StarsView extends ViewGroup {
+public class StarsView extends ViewGroup implements View.OnClickListener {
+    public OnMenuItemClickListener onMenuItemClickListener;
     Point[][] points;
-
+    int radius;
+    MenuPos pos;
+    boolean isOpen = false;
 
     public StarsView(Context context) {
         this(context, null);
@@ -23,18 +39,235 @@ public class StarsView extends ViewGroup {
         super(context, attrs, defStyleAttr);
 
 
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.StarsView, defStyleAttr, 0);
 
+        //radius = a.getInt(R.styleable.StarsView_radius, 5);
+
+        radius = (int) a.getDimension(R.styleable.StarsView_radius,
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics()));
+
+        int iPos = a.getInt(R.styleable.StarsView_pos, 0);
+        switch (iPos) {
+            case 0:
+                pos = MenuPos.left_top;
+                break;
+            case 1:
+                pos = MenuPos.right_top;
+                break;
+            case 2:
+                pos = MenuPos.left_bottom;
+                break;
+            case 3:
+                pos = MenuPos.right_bottom;
+                break;
+        }
+        a.recycle();
     }
 
-
-
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
+    public void setOnMenuItemClickListener(OnMenuItemClickListener onMenuItemClickListener) {
+        this.onMenuItemClickListener = onMenuItemClickListener;
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int n = getChildCount();
+        for (int i = 0; i < n; i++) {
+            View btn = getChildAt(i);
+            btn.setMinimumWidth(20);
+
+            measureChild(btn, widthMeasureSpec, heightMeasureSpec);
+        }
+
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (!changed) return;
+
+        placeMainButton();
+
+        int n = getChildCount();
+        int btnLeft = 0, btnTop = 0;
+        for (int i = 0; i < n - 1; i++) {
+            View btn = getChildAt(i + 1);
+            btn.setVisibility(View.GONE);
+            int cWidth = btn.getMeasuredWidth();
+            int cHeight = btn.getMeasuredHeight();
+
+            double angle = i * Math.PI / 2 / (n - 2);
+
+            btnLeft = (int) (Math.sin(angle) * radius);
+            btnTop = (int) (Math.cos(angle) * radius);
+            if (pos == MenuPos.right_top || pos == MenuPos.right_bottom) {
+                btnLeft = getMeasuredWidth() - cWidth - btnLeft;
+            }
+            if (pos == MenuPos.left_bottom || pos == MenuPos.right_bottom) {
+                btnTop = getMeasuredHeight() - cHeight - btnTop;
+            }
+            btn.layout(btnLeft, btnTop, btnLeft + cWidth, btnTop + cHeight);
+        }
+    }
+
+    private void placeMainButton() {
+        int btnLeft = 0, btnTop = 0;
+        View mainBtn = getChildAt(0);
+        mainBtn.setOnClickListener(this);
+        int width = mainBtn.getMeasuredWidth();
+        int height = mainBtn.getMeasuredHeight();
+
+        switch (pos) {
+            case left_top:
+                btnLeft = 0;
+                btnTop = 0;
+                break;
+            case left_bottom:
+                btnLeft = 0;
+                btnTop = getMeasuredHeight() - height;
+                break;
+            case right_top:
+                btnLeft = getMeasuredWidth() - width;
+                btnTop = 0;
+                break;
+            case right_bottom:
+                btnLeft = getMeasuredWidth() - width;
+                btnTop = getMeasuredHeight() - height;
+                break;
+        }
+        mainBtn.layout(btnLeft, btnTop, btnLeft + width, btnTop + height);
+    }
+
+    @Override
+    public void onClick(View v) {
+        rotateMainBtn(v, 0, 360, 1500);
+        toggleStarsMenu(2500);
+    }
+
+    private void rotateMainBtn(View v, float start, float end, int duration) {
+        RotateAnimation rotateAnimation = new RotateAnimation(start, end,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(duration);
+        rotateAnimation.setFillAfter(true);
+        v.setAnimation(rotateAnimation);
+    }
+
+    public void toggleStarsMenu(int duration) {
+        int n = getChildCount();
+        for (int i = 0; i < n - 1; i++) {
+            final View child = getChildAt(i + 1);
+            child.setVisibility(View.VISIBLE);
+            double angle = Math.PI / 2 / (n - 2);
+            int left = (int) (Math.sin(angle * i) * radius);
+            int top = (int) (Math.cos(angle * i) * radius);
+
+
+            int xFlag = 1;
+            int yFlag = 1;
+            if (pos == MenuPos.left_top || pos == MenuPos.left_bottom)
+                xFlag = -1;
+            if (pos == MenuPos.left_top || pos == MenuPos.right_top)
+                yFlag = -1;
+
+
+            AnimationSet animationSet = new AnimationSet(true);
+            TranslateAnimation translateAnimation = null;
+            if (!isOpen) {
+                translateAnimation = new TranslateAnimation(xFlag * left, 0, yFlag * top, 0);
+                child.setClickable(true);
+                child.setFocusable(true);
+            } else {
+                translateAnimation = new TranslateAnimation(0, xFlag * left, 0, yFlag * top);
+                child.setClickable(false);
+                child.setFocusable(false);
+            }
+            translateAnimation.setFillAfter(true);
+            translateAnimation.setDuration(duration);
+            translateAnimation.setStartOffset((i * 600) / n);
+            translateAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    if (!isOpen) {
+                        child.setVisibility(View.GONE);
+                    }
+                }
+            });
+
+
+            RotateAnimation rotateAnimation = new RotateAnimation(0, 720,
+                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            rotateAnimation.setDuration(duration);
+            rotateAnimation.setFillAfter(true);
+
+            animationSet.addAnimation(rotateAnimation);
+            animationSet.addAnimation(translateAnimation);
+
+            child.startAnimation(animationSet);
+
+            AnimatorSet animatorSet = new AnimatorSet();
+            ObjectAnimator objectAnimator = new ObjectAnimator();
+            objectAnimator.ofFloat(child, "width", 30);
+            animatorSet.play(objectAnimator);
+            animatorSet.start();
+
+            final int index = i + 1;
+            child.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onMenuItemClickListener != null)
+                        onMenuItemClickListener.onMenuItemClick(child, index);
+                    MenuItemClick(index - 1);
+                    isOpen = !isOpen;
+                }
+            });
+        }
+
+        isOpen = !isOpen;
+    }
+
+    private void MenuItemClick(int index) {
+        int duration = 1000;
+        int childCount = getChildCount();
+        for (int j = 0; j < childCount - 1; j++) {
+            View cv = getChildAt(j + 1);
+            AnimationSet animationSet1 = new AnimationSet(true);
+            AlphaAnimation alphaAnimation = new AlphaAnimation(1f, 0f);
+            ScaleAnimation scaleAnimation = null;
+            if (j == index) {
+                scaleAnimation = new ScaleAnimation(1f, 4f, 1f, 4f,
+                        Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+            } else {
+                scaleAnimation = new ScaleAnimation(1f, 0f, 1f, 0f,
+                        Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            }
+            animationSet1.addAnimation(scaleAnimation);
+            animationSet1.addAnimation(alphaAnimation);
+            animationSet1.setDuration(duration);
+            animationSet1.setFillAfter(true);
+            cv.startAnimation(animationSet1);
+
+        }
+    }
+
+    public enum MenuPos {
+        left_top, right_top, left_bottom, right_bottom
+    }
+
+    public interface OnMenuItemClickListener {
+        void onMenuItemClick(View v, int pos);
     }
 }
